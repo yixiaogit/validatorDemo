a6b43d561867f03c339dcc7f9aad7e5687f9daec
xiaoyi <xiao1_mail@163.com>
2022-05-19 23:43:30 +0800

    jdbc数据驱动切换、测试接口调整

--- src/main/java/cn/xiaoyi/validatordemo/controller/TestController.java
+++ src/main/java/cn/xiaoyi/validatordemo/controller/TestController.java
@@ -1,16 +1,31 @@
 package cn.xiaoyi.validatordemo.controller;
 
 import cn.xiaoyi.validatordemo.entity.Result;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Pattern;
 
 @RestController
 @RequestMapping("test")
+@Validated
 public class TestController {
 
-    @PostMapping("validatedTest")
-    public Result validatedTest(){
-        return Result.success();
+
+
+    /**
+     * 正则表达式
+     * @param year
+     * @return
+     */
+    @PostMapping("testRegexp")
+    public Result validatedTest(@Pattern (regexp = "^[\\d]{4}$",message = "年份日期不合法！")@RequestParam("year") String year){
+        return Result.success("参数合法",year);
+
     }
+
+
 }
